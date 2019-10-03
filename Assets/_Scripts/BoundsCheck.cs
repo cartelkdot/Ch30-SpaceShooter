3b6835d98eb5276cd9070c96de56a324c775f5ff
cartelkdot <47113944+cartelkdot@users.noreply.github.com>
2019-10-03 01:57:06 -0400

    did a lot of code.
    See Title.

--- Assets/_Scripts/BoundsCheck.cs
+++ Assets/_Scripts/BoundsCheck.cs
@@ -18,6 +18,8 @@ public class BoundsCheck : MonoBehaviour
     public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     // Start is called before the first frame update
     void Awake()
@@ -31,32 +33,39 @@ public class BoundsCheck : MonoBehaviour
     {
         Vector3 pos = transform.position;
         isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
         if(pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
             isOnScreen = false;
+            offRight = true;
         }
         if(pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
             isOnScreen = false;
+            offLeft = true;
         }
         if(pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
             isOnScreen = false;
+            offUp = true;
         }
         if(pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
             isOnScreen = false;
+            offDown = true;
         }
 
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
         if(keepOnScreen && !isOnScreen)
         {
             transform.position = pos;
             isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
         }
         
     }
