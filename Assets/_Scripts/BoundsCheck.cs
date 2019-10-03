ecf147b64a05049c3d24be7b3277c4ee15e74bb5
cartelkdot <47113944+cartelkdot@users.noreply.github.com>
2019-10-03 00:30:13 -0400

    working on BoundsCheck
    See title.

---
+++ Assets/_Scripts/BoundsCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//To type the next 4 lines, start by typing /// and the nTab.
+/// <summary>
+/// Keeps a GameObject on screen.
+/// Note that this only works for a n orthographic Main Camera at [0,0,0]
+/// </summary>
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Set In Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if(pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+
+    }
+}
